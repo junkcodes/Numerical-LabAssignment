af6790cac61c1adfdc85bb824cfc251b46390c21
Rezwan Mahmud Faisal <faisalrony150@gmail.com>
2019-10-30 02:52:46 +0600

    Create Seidel.m

---
+++ Seidel.m
@@ -0,0 +1,25 @@
+a = [4,1,2,-1;3,6,-1,2;2,-1,5,-3;4,1,-3,-8];
+b = [2;-1;3;2];
+n = length(b);
+x = zeros(n,1);
+t = 0.000005;
+for it = 1 : 100
+  convergence = true;
+  for e = 1 : n
+    sum = 0;
+    for s = 1 : n
+      if s != e
+        sum += a(e,s)*x(s);
+      endif
+    endfor
+    if abs((b(e)-sum)/a(e,e) - x(e)) > tol
+      convergence = false;
+    endif
+    x(e) = (b(e)-sum)/a(e,e);
+    printf("%.5f ",x(e));
+  endfor
+  printf("\n");
+  if convergence
+    break
+  endif
+endfor
